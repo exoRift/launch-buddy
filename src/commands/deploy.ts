26ef330063e0fb17273f777e2096ca465917f73a
Arthur Neuman <arthurbneuman@gmail.com>
2023-08-15 14:50:08 -0400

    Touch up deployment and add a confirmation

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -24,22 +24,23 @@ async function deployAction ({ c: configPath }: Arguments): Promise<void> {
         limit: 1
       }
     })
-      .then(({ data }) => data)
+      .then(({ data: [{ deploy }] }) => deploy)
 
     const repo: string = (await service).repo.split('/').slice(-2).join('/')
 
     const commits = fetcher.github.get(`/repos/${repo}/commits`, {
       params: {
-        since: (await lastDeploy).createdAt
+        since: (await lastDeploy).commit.createdAt,
+        sha: (await service).branch
       }
     })
-      .then(({ data }) => data)
+      .then(({ data }) => data.slice(1))
 
     return [s, await commits]
   }))
 
-  for (const [name, update] of updates) {
-    console.log(`${chalk.blueBright(name)} - ${chalk.redBright(update.length)} commits since the last deployment`)
+  for (const [service, update] of updates) {
+    console.log(`${chalk.blueBright(service.name)} - ${chalk.redBright(update.length)} commits since the last deployment`)
 
     for (const commit of update) {
       console.log('\t', chalk.gray(commit.commit.message.split('\n')[0]))
@@ -55,11 +56,21 @@ async function deployAction ({ c: configPath }: Arguments): Promise<void> {
       value: s
     }))
   }])
-    .then((answers: (typeof config)['services']) => {
-      for (const answer of answers) {
-        void fetcher.render.post(`/services/${answer.serviceID}/deploys`)
-          .then(() => console.log(chalk.greenBright(`${answer.name} deployment triggered!`)))
-      }
+    .then(({ deploy: answers }: { deploy: (typeof config)['services'] }) => {
+      return inquirer.prompt({
+        type: 'confirm',
+        name: 'confirm',
+        message: `You will update: ${chalk.yellowBright(answers.map((s) => s.name).join(', '))}. Are you sure?`,
+        default: false
+      })
+        .then(({ confirm }) => {
+          if (confirm) {
+            for (const answer of answers) {
+              void fetcher.render.post(`/services/${answer.serviceID}/deploys`)
+                .then(() => console.log(chalk.greenBright(`${answer.name} deployment triggered!`)))
+            }
+          } else console.log(chalk.redBright('Deployment aborted.'))
+        })
     })
 }
 
